871e362b22a92c464c0a2ba0d6d842d6175212b7
Dorota <d.a.lewinska@gmail.com>
2020-10-27 12:15:04 +0000

    Midway through exercise004.(Punctuation not solved)

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -2,6 +2,19 @@
 
 function pigLatin(str){
     // You code here!
+    // turn into arrays with separate words
+    const arrWords = str.split(' ');
+    const myArray = [];
+    //loop through words
+    for (const word of arrWords){
+        let wordEnd = word.substring(0, 1) + 'ay';
+        let wordStart = word.substring(1) ;
+        //console.log(wordEnd);
+        myArray.push(wordStart+wordEnd);
+    }
+
+    //console.log(myArray);
+    return myArray.join(' ');
 }
 
 module.exports = {
